f39bb3a4c63e7b8c9a8e8601feec93f705aed096
Peter <parasianchr@gmail.com>
2026-02-01 10:38:37 +0700

    FIX_v5

--- MyAPP/Models/Preset.cs
+++ MyAPP/Models/Preset.cs
@@ -11,5 +11,20 @@
         public System.DateTime CreatedAt { get; set; }
 
         public System.Collections.Generic.List<Template> Templates { get; set; } = new System.Collections.Generic.List<Template>();
+
+        // Modifikasi: Menambahkan properti VariableCount untuk menghitung total variabel dari semua templat
+        public System.Int32 VariableCount
+        {
+            get
+            {
+                if (this.Templates == null)
+                {
+                    return 0;
+                }
+
+                // Menggunakan Fully Qualified Name untuk Linq Sum agar hemat memori (tidak perlu using tambahan)
+                return System.Linq.Enumerable.Sum(this.Templates, t => t.Variables?.Count ?? 0);
+            }
+        }
     }
 }
